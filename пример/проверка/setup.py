b49a925a771e9c313a55c7f59e0bdcd043d98179
Xinboshin <106208768+Xinboshin@users.noreply.github.com>
2023-01-09 23:36:26 +0300

    Update and rename example/validation/setup.py to пример/проверка/setup.py

--- пример/проверка/setup.py
+++ пример/проверка/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-	name = 'marrow.script-validation-example',
+	name = 'xinboshin.script-validation-example',
 	version = '1.0',
 	py_modules = ['validation'],
 	include_package_data = False,
-	install_requires = ['marrow.script'],
+	install_requires = ['xinboshin.script'],
 	entry_points = {'console_scripts': ['validation = validation:main']}
 )
